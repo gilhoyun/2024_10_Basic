dcb98a55bc9eac9775cd00fb279e0de7cec74905
gilhoyun <gilhoyun04@gmail.com>
2024-10-08 15:17:44 +0900

    게시판 틀잡기

---
+++ src/com/koreaIT/BAM/Main.java
@@ -0,0 +1,26 @@
+package com.koreaIT.BAM;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+			System.out.println("=== 프로그램 시작 ===");
+			
+			Scanner sc = new Scanner(System.in);
+			
+	     while (true) {
+	    	System.out.print("명령어) ");
+			String cmd = sc.nextLine();
+			
+			if(cmd.equals("exit")) {
+				
+				break;
+			}
+		}
+	     
+	    sc.close();
+	    
+		System.out.println("=== 프로그램 종료 ===");
+	}
+}
